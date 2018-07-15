82f9fe3959547d3a863ff5c4fb9ec0391de23635
April Nichols <nicholsar@gmail.com>
2018-07-15 18:58:54 -0500

    basic component setup

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import AllPhotos from '../components/AllPhotos/AllPhotos';
+import Home from '../components/Home/Home';
+import MyCollection from '../components/MyCollection/MyCollection';
+import Pics from '../components/Pics/Pics';
+import SinglePic from '../components/SinglePic/SinglePic';
 import './App.css';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Home />
+        <AllPhotos />
+        <MyCollection />
+        <Pics />
+        <SinglePic />
       </div>
     );
   }
---
+++ src/components/MyCollection/MyCollection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MyCollection.css';
+
+class MyCollection extends React.Component {
+  render () {
+    return (
+      <div className="MyCollection">
+        <h1>My Collection</h1>
+      </div>
+    );
+  }
+}
+
+export default MyCollection;
