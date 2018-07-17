cb6863ace79a496029d3ff023b72fcd7da6383c1
April Nichols <nicholsar@gmail.com>
2018-07-16 21:17:11 -0500

    retrieving all pic names from firebase

---
+++ src/firebaseReq/pics.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import constants from '../constants';
+
+const getAllPics = () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${constants.firebaseConfig.databaseURL}/pics.json`)
+      .then((res) => {
+        const allPics = [];
+        if (res.data !== null) {
+          Object.keys(res.data).forEach(key => {
+            res.data[key].id = key;
+            allPics.push(res.data[key]);
+          });
+        }
+        resolve(allPics);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+export default { getAllPics };
