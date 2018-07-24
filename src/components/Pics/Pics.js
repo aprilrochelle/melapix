d181564bb9af478824973d9937174ec86e243ca9
April Nichols <nicholsar@gmail.com>
2018-07-23 23:34:21 -0500

    modal styling - PROJECT MVP

--- src/components/Pics/Pics.js
+++ src/components/Pics/Pics.js
@@ -6,13 +6,17 @@ class Pics extends React.Component {
     this.props.addToMyPics(this.props.details);
   }
 
+  modalClick = (image) => {
+    this.props.onClick(image);
+  }
+
   render () {
     const { details } = this.props;
     const image = require(`./../../images/${details.image}`);
     return (
       <div className="Pics col-sm-3">
         <div className="pic-card">
-          <img className="picture" src={image} alt={details.name}/>
+          <img className="picture" src={image} alt={details.name} onClick={() => this.modalClick(image)}/>
           <h4>{details.name}</h4>
           <button
             className="btn btn-success"
