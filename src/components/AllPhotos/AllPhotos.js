fd18c6fcfce48238e07dee28ae296741a67de5b9
April Nichols <nicholsar@gmail.com>
2018-08-06 00:17:09 -0500

    alerts working

--- src/components/AllPhotos/AllPhotos.js
+++ src/components/AllPhotos/AllPhotos.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Modal from 'react-responsive-modal';
 import moment from 'moment';
+import AlertDismissable from '../Alerts/Alerts';
 import auth from '../../firebaseReq/auth';
 import myPics from '../../firebaseReq/myPics';
 import picRequests from '../../firebaseReq/pics';
@@ -14,23 +15,12 @@ class AllPhotos extends React.Component {
     originalState: [],
     open: false,
     picPreview: '',
+    showAlert: false,
+    alertText: '',
   }
 
   addToMyPics = (imageDetails) => {
-    const totalInfo = {};
-    totalInfo.picId = imageDetails.id;
-    totalInfo.name = imageDetails.name;
-    totalInfo.photogId = imageDetails.photogId;
-    totalInfo.total = 0;
-    favorited
-      .postTotal(totalInfo.picId, totalInfo)
-      .then(() => {
-
-      })
-      .catch(err => {
-        console.error('error in posting to totals', err);
-      });
-
+    //  Create newImage object to post to user collection.
     const newImage = {};
     newImage.name = imageDetails.name;
     newImage.image = imageDetails.image;
@@ -39,10 +29,38 @@ class AllPhotos extends React.Component {
     newImage.uid = auth.getUid();
     newImage.photogId = imageDetails.photogId;
     newImage.dateAdded = moment().format('MMM Do YY');
-    myPics
-      .postRequest(newImage.uid, newImage)
-      .then(() => {
 
+    //  But first, filter through user's collection to check for a match.
+    myPics
+      .getMyPics(newImage.uid)
+      .then(pics => {
+        const filteredPics = pics.filter(pic => {
+          return (pic.picId === newImage.picId);
+        });
+
+        //  If a pic in the user's collection matces the selected image's id, show an alert and prevent saving again.
+        if (filteredPics.length > 0) {
+          this.setState({showAlert: true, alertText: 'You have already purchased this image. Please visit My Collection to view.'});
+        } else {
+          // If no match is found in the existing collection, post the image to the collection.
+          myPics
+            .postRequest(newImage)
+            .then(() => {
+              this.setState({showAlert: true, alertText: 'Image Saved.'});
+
+              //  After posting to user's collection, post to totals collection.
+              const totalInfo = {};
+              totalInfo.picId = imageDetails.id;
+              totalInfo.name = imageDetails.name;
+              totalInfo.photogId = imageDetails.photogId;
+              totalInfo.total = 0;
+              favorited
+                .postTotal(totalInfo.picId, totalInfo)
+                .then(() => {
+
+                });
+            });
+        }
       })
       .catch((err) => {
         console.error('error in order post', err);
@@ -97,6 +115,10 @@ class AllPhotos extends React.Component {
     this.setState({ open: false });
   };
 
+  onDismiss = () => {
+    this.setState({showAlert: false});
+  }
+
   render () {
     const { open } = this.state;
     const picComponents = this.state.pics.map((pic) => {
@@ -113,6 +135,11 @@ class AllPhotos extends React.Component {
     return (
       <div className="AllPhotos col-md-12">
         <h1>Search Photos</h1>
+        <AlertDismissable
+          text={this.state.alertText}
+          showAlert={this.state.showAlert}
+          onDismiss={this.onDismiss}
+        />
         <div className="row">
           <label htmlFor="search-terms" className="col-sm-6 col-sm-offset-3">
             <input type="text" id="search-terms" className="search col-sm-8 col-sm-offset-2" onKeyUp={this.filterPics} onKeyPress={this.filterPicsBackspace} />
