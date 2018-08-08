9755760fed9911fb700bf6d20fdb616970a0da9d
April Nichols <nicholsar@gmail.com>
2018-08-07 23:56:35 -0500

    msg to photog if no work has been saved yet

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -14,10 +14,10 @@ class Dashboard extends React.Component {
     favorited
       .getFavTotals()
       .then((savedPics) => {
-        savedPics.filter(savedPic => {
+        const mySavedWork = savedPics.filter(savedPic => {
           return (savedPic.photogId === myId);
         });
-        this.setState({ data: savedPics });
+        this.setState({ data: mySavedWork });
       })
       .catch((err) => {
         console.error(err);
@@ -36,34 +36,42 @@ class Dashboard extends React.Component {
     });
 
     let photogDisplay;
-    if (data) {
-      photogDisplay = (
-        <div className="photog-display">
-          <h4>Here's a list of your pics that have been saved by users:</h4>
-          {picList}
-        </div>
+    (data.length > 0) ?
+      (
+        photogDisplay = (
+          <div className="photog-display">
+            <h4>Here's a list of your pics that have been saved by users:</h4>
+            {picList}
+          </div>
+        )
+      ) : (
+        photogDisplay = (
+          <div className="photog-display">
+            <h4>No pics have been saved by users yet!</h4>
+          </div>
+        )
       );
-    } else {
-      photogDisplay = (
-        <div className="photog-display">
-          <h4>No pics have been saved by users yet!</h4>
-        </div>
-      );
-    }
 
     return (
       <div className="Dashboard col-md-12">
         <h1>My Dashboard</h1>
-        <ResponsiveContainer width="100%" height={500}>
-          <BarChart width={800} height={500} data={this.state.data}>
-            <XAxis dataKey="picId" stroke="#717171" />
-            <YAxis />
-            <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#fff' }} />
-            <Legend width={100} wrapperStyle={{ top: 30, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
-            <CartesianGrid stroke="#ccc" fill="#163b34" strokeDasharray="5 5" />
-            <Bar type="monotone" dataKey="total" fill="#69ab0b" barSize={30} />
-          </BarChart>
-        </ResponsiveContainer>
+        {
+          (data.length > 0) ?
+            (
+              <ResponsiveContainer width="100%" height={500}>
+                <BarChart width={800} height={500} data={this.state.data}>
+                  <XAxis dataKey="picId" stroke="#717171" />
+                  <YAxis />
+                  <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#fff' }} />
+                  <Legend width={100} wrapperStyle={{ top: 30, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+                  <CartesianGrid stroke="#ccc" fill="#163b34" strokeDasharray="5 5" />
+                  <Bar type="monotone" dataKey="total" fill="#69ab0b" barSize={20} />
+                </BarChart>
+              </ResponsiveContainer>
+            ) : (
+              null
+            )
+        }
         {photogDisplay}
       </div>
     );
